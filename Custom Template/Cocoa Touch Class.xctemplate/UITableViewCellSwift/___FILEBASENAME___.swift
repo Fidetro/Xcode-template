fe6f866b706e1284731a88b7f740e29626f246d9
fidetro <zykzzzz@hotmail.com>
2017-11-02 11:02:49 +0800

    update swift

--- Custom Template/Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
+++ Custom Template/Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
@@ -4,15 +4,40 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
 
+    static func dequeueReusableWithTableView(tableView:UITableView) -> ___FILEBASENAMEASIDENTIFIER___ {
+        let reusableCell = tableView.dequeueReusableCell(withIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier())
+        if  let cell = reusableCell {
+            return cell as! ___FILEBASENAMEASIDENTIFIER___
+        }else{
+            let cell = ___FILEBASENAMEASIDENTIFIER___.init(style: .default, reuseIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier())
+            return cell
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        snpLayoutSubview()
+    }
+    
+    func snpLayoutSubview() {
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    static func identifier() -> String {
+        return "k___FILEBASENAMEASIDENTIFIER___Identifier"
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
