48afc0306ef74a18fcf2ad802b607ed68a808560
fidetro <zykzzzz@hotmail.com>
2017-12-25 17:58:50 +0800

    updateCollectionReusableView

--- Custom Template/Cocoa Touch Class.xctemplate/UICollectionReusableViewSwift/___FILEBASENAME___.swift
+++ Custom Template/Cocoa Touch Class.xctemplate/UICollectionReusableViewSwift/___FILEBASENAME___.swift
@@ -3,5 +3,23 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    static let identifier = "k___FILEBASENAMEASIDENTIFIER___Identifier"
+    static func dequeueReusable(withCollectionView collectionView:UICollectionView,
+                                for indexPath:IndexPath) -> ___FILEBASENAMEASIDENTIFIER___ {
+        return collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionElementKindSectionFooter,
+            withReuseIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier,
+            for: indexPath) as! ___FILEBASENAMEASIDENTIFIER___
+    }
+    override init(frame: CGRect) {
+        super.init(frame: CGRect.zero)
+        snpLayoutSubview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func snpLayoutSubview() {
         
+    }
 }
