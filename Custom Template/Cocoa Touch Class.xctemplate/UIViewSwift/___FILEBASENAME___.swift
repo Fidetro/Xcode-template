02b4129e71202743fbc90e219d915714c1118eed
fidetro <zykzzzz@hotmail.com>
2018-03-19 09:35:15 +0800

    update Swift UIView

--- Custom Template/Cocoa Touch Class.xctemplate/UIViewSwift/___FILEBASENAME___.swift
+++ Custom Template/Cocoa Touch Class.xctemplate/UIViewSwift/___FILEBASENAME___.swift
@@ -4,12 +4,16 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init() {
+        super.init(frame: .zero)
+        snpLayoutSubview()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func snpLayoutSubview() {
+        
+    }
 }
