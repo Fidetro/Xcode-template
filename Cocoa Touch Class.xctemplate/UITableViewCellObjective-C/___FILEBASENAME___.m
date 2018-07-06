d40ee806a7f49359b2de2b044297c1dc687b15bd
Fidetro <zykzzzz@hotmail.com>
2018-07-06 16:14:10 +0800

    delete overwrite method

--- Cocoa Touch Class.xctemplate/UITableViewCellObjective-C/___FILEBASENAME___.m
+++ Cocoa Touch Class.xctemplate/UITableViewCellObjective-C/___FILEBASENAME___.m
@@ -4,15 +4,5 @@
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 
 @end
