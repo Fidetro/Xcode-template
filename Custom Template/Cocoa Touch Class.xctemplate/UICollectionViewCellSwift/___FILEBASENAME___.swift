e9ea30f44d05e0256b7c22a36110430bcb086e36
fidetro <zykzzzz@hotmail.com>
2019-02-25 20:24:09 +0800

    remove UICollectionView and UITableView,use extension

--- Custom Template/Cocoa Touch Class.xctemplate/UICollectionViewCellSwift/___FILEBASENAME___.swift
+++ Custom Template/Cocoa Touch Class.xctemplate/UICollectionViewCellSwift/___FILEBASENAME___.swift
@@ -3,26 +3,16 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
-    static let identifier = "k___FILEBASENAMEASIDENTIFIER___Identifier"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         snpLayoutSubview()
     }
     
-    
-    static func dequeueReusable(with collectionView:UICollectionView,for indexPath:IndexPath) -> ___FILEBASENAMEASIDENTIFIER___ {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier, for: indexPath) as! ___FILEBASENAMEASIDENTIFIER___
-    }
-  
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-
-    
-
 }
 
 //MARK: setup view
--- Custom Template/Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
+++ Custom Template/Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
@@ -4,18 +4,6 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
     
-    static let identifier = "k___FILEBASENAMEASIDENTIFIER___Identifier"
-
-    static func dequeueReusable(with tableView:UITableView) -> ___FILEBASENAMEASIDENTIFIER___ {
-        let reusableCell = tableView.dequeueReusableCell(withIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier)
-        if  let cell = reusableCell as? ___FILEBASENAMEASIDENTIFIER___ {
-            return cell
-        }else{
-            let cell = ___FILEBASENAMEASIDENTIFIER___.init(style: .default, reuseIdentifier: ___FILEBASENAMEASIDENTIFIER___.identifier)
-            return cell
-        }
-    }
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
