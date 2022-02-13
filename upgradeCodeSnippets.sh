89c7ca66e55ad0077153be12ba60561d56e588b0
Fidetro <zykzzzz@hotmail.com>
2022-02-13 12:54:06 +0800

    upgrade Template

--- upgradeCodeSnippets.sh
+++ upgradeCodeSnippets.sh
@@ -5,5 +5,8 @@ echo "文件夹不存在"
 else
 cp -r ~/Library/Developer/Xcode/UserData/CodeSnippets ./CodeSnippets
 cp -r ./CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+git add *
+git commit -m "sync CodeSnippets"
+git push origin master
 echo "复制完成"
 fi
--- upgradeTemplate.sh
+++ upgradeTemplate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-path="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/iOS/Source/Cocoa\ Touch\ Class.xctemplate"
-if [ ! -d"${path}" ];then
+src_path="./Custom Template/Cocoa Touch Class.xctemplate/"
+tar_path="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/iOS/Source/Cocoa Touch Class.xctemplate"
+if [ ! -d"${tar_path}" ];then
 echo "文件夹不存在"
 else
-echo 'cp -r ./Custom\ Template/Cocoa\ Touch\ Class.xctemplate/. $path'
+sudo cp -r "${src_path}" "${tar_path}"
 git add *
 git commit -m "upgrade Template"
 git push origin master
